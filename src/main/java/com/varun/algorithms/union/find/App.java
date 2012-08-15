b05eb4589e9c3d7de2367dd3c94f93ca16fafb6a
Varun Vontimitta <varun.vontimitta@gmail.com>
2012-08-15 08:10:43 -0700

    Initializing git project for union-find algorithms

---
+++ src/main/java/com/varun/algorithms/union/find/App.java
@@ -0,0 +1,13 @@
+package com.varun.algorithms.union.find;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
